3ed573cc2da13f8a39488d3ae3f0b52b16956c17
sivanand83 <saisivanands19@gmail.com>
2024-03-05 12:49:44 +0530

    autocomple attribute is add in render_kw

--- app/zForms.py
+++ app/zForms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, EqualTo, Regexp, Email
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(message="- Enter a valid Email")],
-                        render_kw={"placeholder": "Enter Email"})
+                        render_kw={"placeholder": "Enter Email", "autocomplete":"off"})
     
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=18, message="- Password should be between greter than 8")], render_kw={"placeholder": "Enter your password"})
 
@@ -13,9 +13,9 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     user_name = StringField('Username', validators=[DataRequired(), Regexp(r'^[a-zA-Z0-9_-]+$', message='- Allow only (a-zA-Z0-9_-)')], 
                                                     filters=[lambda x: x.lower() if x is not None else x], 
-                                                    render_kw={"placeholder": "Username"})
+                                                    render_kw={"placeholder": "Username", "autocomplete":"off"})
     email = StringField('Email', validators=[DataRequired(), Email(message="- Enter a valid Email")],
-                        render_kw={"placeholder": "Enter Email"})
+                        render_kw={"placeholder": "Enter Email", "autocomplete":"off"})
     password = PasswordField('Password', validators=[DataRequired(),
                                                      Regexp(r'^[a-zA-Z0-9_!@#$%^&*()-]{8,18}$', message="- Invalid Password Format")], 
                                                      render_kw={"placeholder": "Enter your password"})
